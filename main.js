ac6683e77ee82bbc36f108c52984789fcf480db6
sourob14202 <sourob14202@gmail.com>
2021-10-22 01:01:57 +0600

    chill

---
+++ main.js
@@ -0,0 +1,17 @@
+AOS.init();
+(function ($) {
+  "use strict";
+
+  $(function () {
+    var header = $(".start-style");
+    $(window).scroll(function () {
+      var scroll = $(window).scrollTop();
+
+      if (scroll >= 10) {
+        header.removeClass("start-style").addClass("scroll-on");
+      } else {
+        header.removeClass("scroll-on").addClass("start-style");
+      }
+    });
+  });
+})(jQuery);
